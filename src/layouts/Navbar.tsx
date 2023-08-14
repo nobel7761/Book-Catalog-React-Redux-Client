f2c35a187739b86220ceda4a64aa05abd92b92f5
nobel7761 <habiburnobel@gmail.com>
2023-08-14 18:39:26 +0600

    footer completed

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       <div className="relative z-20">
         <div className="flex justify-between items-center px-8 py-5 space-x-5">
           <div>
-            <Link to="/">
+            <Link to="/" className="flex items-center gap-x-2">
               <img
                 className="h-10 w-auto"
                 src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/128/book-bookmark-icon.png"
@@ -29,28 +29,28 @@ const Navbar = () => {
                 height="128"
                 alt="logo"
               />
+              <span className="text-[#1ABC9C] text-2xl uppercase font-bold">
+                Book Catalog
+              </span>
             </Link>
           </div>
           <div className="-mr-2 -my-2 md:hidden"></div>
           <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
-            <div className="flex space-x-4">
+            <div className="flex space-x-4"></div>
+            <div className="flex items-center ml-12 gap-x-8">
               {/* all books */}
               <Link
                 to="/all-books"
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
+                className="text-base font-medium text-gray-500 hover:text-[#1ABC9C]"
               >
                 All Books
               </Link>
-            </div>
-            <div className="flex items-center ml-12 gap-x-8">
               {/* add new book */}
               <Link
                 to="/add-new-book"
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
+                className="text-base font-medium text-gray-500 hover:text-[#1ABC9C]"
               >
-                <button className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                  Add New Book
-                </button>
+                Add New Book
               </Link>
 
               {/* sign in button */}
